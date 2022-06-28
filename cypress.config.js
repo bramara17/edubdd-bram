7079e37d49e2b1b9b7da257f18e3f702a4e3bd39
bramara_ <bramara.sambada.17@gmail.com>
2022-06-28 23:04:39 +0700

    Upload project bdd

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    "video": false,
+    "defaultCommandTimeout": 5000,
+    "pageLoadTimeout": 10000,
+    "excludeSpecPattern": ["*.js", "*.md "],
+    "specPattern": "**/*.feature"
+}
+});
---
+++ cypress/e2e/search.feature
@@ -0,0 +1,9 @@
+Feature: Search in Website Zero Bank
+
+    As a guest
+    I want to search into Zero Bank Website
+
+Scenario: Search
+    Given I open Zero Bank Website Homepage
+    When I type halo in search input
+    Then I should see the result page that shows search result
